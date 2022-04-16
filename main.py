46fbed34be716d68b97299861d7b7275fb104df5
vishnyakovalexei1973 <vishnyakovalexei1973@gmail>
2022-04-16 19:07:13 +0300

    3

--- main.py
+++ main.py
@@ -1,4 +1,11 @@
-print("What is Your name?")
-name = input()
-print("Hi,", name)
-
+print("+,-,*,/?")
+a=input('input operation')
+b=input('input first jperand')
+c=input('input cecond operand')
+if a=='+' :
+    print(b+c)
+elif a=='-':
+    print(b-c)
+elif a=='*' :
+    print(b*c)
+elif a==
